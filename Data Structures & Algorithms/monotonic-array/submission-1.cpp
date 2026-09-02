0dca1d2470b3c2b6caf34533ec10dc97021451f7
Nguyễn Lê Ngọc Duy <ngocduywork3112@gmail.com>
2026-09-02 15:24:49 +0700

    Add: monotonic-array - submission-1

---
+++ Data Structures & Algorithms/monotonic-array/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true, decreasing = true;
+        int n = nums.size();
+        for (int i = 0; i < n - 1; ++i) {
+            if (!(nums[i] <= nums[i + 1])) increasing = false;
+            if (!(nums[i] >= nums[i + 1])) decreasing = false;
+        }
+        return increasing || decreasing;
+    }
+};
