050ec6f3f98d048ed5a2df43ff8d2f54a51d7d96
Nguyễn Lê Ngọc Duy <ngocduywork3112@gmail.com>
2026-09-07 19:04:01 +0700

    Add: divide-array-into-equal-pairs - submission-0

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int> oddSet;
+        for (int num: nums) {
+            if (oddSet.count(num)) {
+                oddSet.erase(num);
+            } else {
+                oddSet.insert(num);
+            }
+        }
+        return oddSet.empty();
+    }
+};
