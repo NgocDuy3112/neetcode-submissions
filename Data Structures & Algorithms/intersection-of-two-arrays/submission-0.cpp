281eb0ffd740cb578c20fbd09c15b685a3656b41
Nguyễn Lê Ngọc Duy <ngocduywork3112@gmail.com>
2026-09-14 09:27:08 +0700

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s1, s2;
+        vector<int> res;
+        for (int num: nums1) s1.insert(num);
+        for (int num: nums2) s2.insert(num);
+        for (int num: s1) {
+            if (s2.find(num) != s2.end()) res.push_back(num);
+        }
+        return res;
+    }
+};
