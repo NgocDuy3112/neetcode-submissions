b8bfbeb45a5e8ce870153c79dc7c78a58ba77f3f
Nguyễn Lê Ngọc Duy <ngocduywork3112@gmail.com>
2026-09-24 14:31:16 +0700

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+        for (int r = 1; r < m; r++) dp[r][0] = dp[r - 1][0];
+        for (int c = 1; c < n; c++) dp[0][c] = dp[0][c - 1];
+        for (int r = 1; r < m; r++) {
+            for (int c = 1; c < n; c++) {
+                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
